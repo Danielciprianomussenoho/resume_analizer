162b6615ee65f6d74b2ea31dd488cfc8fc525d8f
Daniel cipriano mussenoho <Danielciprianomussenoho@gmail.com>
2025-07-29 05:10:34 +0100

    cloud storage, pdf-image conversion , key-value-storage and ai feedback

---
+++ app/lib/utils.ts
@@ -0,0 +1,15 @@
+
+
+
+export function  formatSize(bytes: number){
+    if(bytes === 0) return "0 bytes";
+
+    const k = 1024;
+    const size = ["bytes","KB","MB","GB","TB"];
+
+    const  i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + " " + size[i];
+}
+
+export const generateUUID = () => crypto.randomUUID();
